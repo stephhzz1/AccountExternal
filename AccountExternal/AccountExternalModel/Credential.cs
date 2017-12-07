b2b3e9a5543f23b94bd745aa4e880c7fd45fefdf
Cris Dismaya <dismaya.marcelino20@gmail.com>
2017-12-07 09:34:28 +0800

    Create Grid CRUD in Credential

--- AccountExternal/AccountExternalModel/Credential.cs
+++ AccountExternal/AccountExternalModel/Credential.cs
@@ -8,7 +8,7 @@ namespace AccountExternalModel
         public bool IsActive { get; set; }
 
         public int CredentialId { get; set; }
-        
+        public string Credentialname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Salt { get; set; }
