df513ce5291de68ff7a6b934e5945d33622f7a80
Cris Dismaya <dismaya.marcelino20@gmail.com>
2018-01-09 14:35:59 +0800

    2018-01-09 API Controller

---
+++ AccountExternal/AccountExternalWeb/ApiControllers/TestApiController.cs
@@ -0,0 +1,30 @@
+﻿using ExternalAccountWebAuthentication.Authentication;
+using System;
+using System.Web.Http;
+
+namespace AccountExternalWeb.ApiControllers
+{
+    [RoutePrefix("TestApi")]
+    public class TestApiController : BaseApiController
+    {
+        [Route("Get")]
+        public IHttpActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        [Route("RoleTest")]
+        [ApiAuthorizationFilterAttribute(false, new string[] { "ExternalAccountAdministrator" })]
+        public IHttpActionResult RoleTest()
+        {
+            try
+            {
+                return Ok(CredentialId);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
